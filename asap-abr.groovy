68cc7fc287c588e7b615dcca394e161a28f01311
jp589 <68749206+jp589@users.noreply.github.com>
2024-07-08 12:41:18 -0400

    Uploaded files necessary for singularity image.

---
+++ asap-abr.groovy
@@ -0,0 +1,331 @@
+// always invoke this script via $ASAP_HOME/bin/groovy
+
+/**********************
+ *** Script Imports ***
+**********************/
+
+
+import java.nio.file.*
+import java.time.*
+import groovy.json.*
+import groovy.util.CliBuilder
+import org.slf4j.*
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import bio.comp.jlu.asap.api.DataType
+import bio.comp.jlu.asap.api.FileType
+
+import static bio.comp.jlu.asap.api.GenomeSteps.*
+import static bio.comp.jlu.asap.api.Paths.*
+
+
+
+
+/************************
+ *** Script Constants ***
+************************/
+final def env = System.getenv()
+ASAP_HOME = env.ASAP_HOME
+CARD = "${ASAP_HOME}/share/card-3.0.7"
+
+PERC_SEQ_IDENT = 0.4
+
+
+
+/*********************
+ *** Script Params ***
+*********************/
+
+
+log = LoggerFactory.getLogger( getClass().getName() )
+
+def cli = new CliBuilder( usage: 'asap-abr.groovy --project-path <project-path> --genome-id <genome-id>' )
+    cli.p( longOpt: 'project-path', args: 1, argName: 'project-path', required: true, 'Path to project directory' )
+    cli.g( longOpt: 'genome-id', args: 1, argName: 'genome-id', required: false, 'Genome ID in config file' )
+def opts = cli.parse( args )
+
+if( !opts?.p ) {
+    log.error( 'no project path provided!' )
+    System.exit( 1 )
+}
+
+def genomeId
+if( opts?.g  &&  opts.g ==~ /\d+/ )
+    genomeId = Integer.parseInt( opts.g )
+else if( env.SGE_TASK_ID  &&  env.SGE_TASK_ID ==~ /\d+/ )
+    genomeId = Integer.parseInt( env.SGE_TASK_ID )
+else {
+    log.error( 'no genome id provided!' )
+    System.exit( 1 )
+}
+
+// log system environment vars and Java properties
+log.info( "SCRIPT: ${getClass().protectionDomain.codeSource.location.path}" )
+log.info( "USER: ${env.USER}" )
+log.info( "CWD: ${env.PWD}" )
+log.info( "HOSTNAME: ${env.HOSTNAME}" )
+log.info( "ASAP_HOME: ${env.ASAP_HOME}" )
+log.info( "PATH: ${env.PATH}" )
+def props = System.getProperties()
+log.info( "script.name: ${props['script.name']}" )
+log.info( "groovy.home: ${props['groovy.home']}" )
+log.info( "file.encoding: ${props['file.encoding']}" )
+log.info( "genome-id: ${genomeId}" )
+
+
+/********************
+ *** Script Paths ***
+********************/
+
+
+Path rawProjectPath = Paths.get( opts.p )
+if( !Files.exists( rawProjectPath ) ) {
+    log.error( "Error: project directory (${rawProjectPath}) does not exist!" )
+    System.exit(1)
+}
+final Path projectPath = rawProjectPath.toRealPath()
+log.info( "project-path: ${projectPath}")
+
+
+// read config json
+Path configPath = projectPath.resolve( 'config.json' )
+if( !Files.isReadable( configPath ) ) {
+    log.error( 'config.json not readable!' )
+    System.exit( 1 )
+}
+final def config = (new JsonSlurper()).parseText( projectPath.resolve( 'config.json' ).text )
+
+
+if( config.project.debugging ) { // set logging to debug upon user request
+    ch.qos.logback.classic.Logger rootLogger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger( org.slf4j.Logger.ROOT_LOGGER_NAME )
+    rootLogger.setLevel( ch.qos.logback.classic.Level.DEBUG )
+}
+
+
+final def genome = config.genomes.find( { it.id == genomeId } )
+if( !genome ) {
+    log.error( "no genome found in config! genome-id=${genomeId}" )
+    System.exit( 1 )
+}
+final String genomeName = "${config.project.genus}_${genome.species}_${genome.strain}"
+log.info( "genome-name: ${genomeName}")
+
+
+final Path abrPath = projectPath.resolve( PROJECT_PATH_ABR )
+Files.createFile( abrPath.resolve( "${genomeName}.running" ) ) // create state.running
+
+
+// sequence path
+final Path genomeSequencePath
+Path scaffoldsPath = Paths.get( projectPath.toString(), PROJECT_PATH_SCAFFOLDS, genomeName, "${genomeName}.fasta" )
+Path sequencePath  = Paths.get( projectPath.toString(), PROJECT_PATH_SEQUENCES, "${genomeName}.fasta" )
+if( Files.isReadable( scaffoldsPath ) ) {
+    genomeSequencePath = scaffoldsPath
+    log.info( "sequence file (scaffolds): ${genomeSequencePath}" )
+} else if( Files.isReadable( sequencePath ) ) {
+    genomeSequencePath = sequencePath
+    log.info( "sequence file: ${genomeSequencePath}" )
+} else
+    terminate( "no sequence file! gid=${genomeId}", abrPath, genomeName )
+
+
+final Path taxPath = projectPath.resolve( "abr" )
+
+// create local tmp directory
+final Path tmpPath = Paths.get( '/', 'var', 'tmp', "tmp-${System.currentTimeMillis()}-${Math.round(Math.random()*1000)}" )
+try { // create tmp dir
+    log.info( "tmp-folder: ${tmpPath}" )
+    Files.createDirectory( tmpPath )
+    if( !config.project.debugging ) {
+        addShutdownHook( {
+            try {
+                tmpPath.deleteDir()
+                // cleanup
+                log.debug( 'delete tmp-dir' )
+            } catch( IOException ex ) {
+                log.error( "could not recursively delete tmp-dir=${tmpPath}", ex )
+            }
+        } )
+    }
+} catch( Throwable t ) {
+    terminate( "could create tmp dir! gid=${genomeId}, tmp-dir=${tmpPath}", t, taxPath, genomeName )
+}
+
+
+// create info object
+def info = [
+    time: [
+        start: OffsetDateTime.now().toString()
+    ],
+    genome: [
+        id: genome.id,
+        species: genome.species,
+        strain: genome.strain
+    ],
+    path: abrPath.toString(),
+    abr: [:]
+]
+
+
+
+
+/********************
+ *** Script Logic ***
+********************/
+
+
+// parse CARD aro terms
+def aroTerms = (new JsonSlurper()).parseText( Paths.get( "${CARD}/aro.json" ).text )
+
+// Name of the conda environment that runs rgi
+def condaEnvName = 'rgi'
+
+
+// process
+
+Path localGenomeSequencePath = tmpPath.resolve("${genomeName}.fasta")
+Files.copy( genomeSequencePath, localGenomeSequencePath )
+String cardOutput = 'card'
+
+// RGI needs localDB files in the tmp directory
+
+def tmpDir = Paths.get(tmpPath.toString())
+def localDBDir = tmpDir.resolve("localDB")
+Files.createDirectories(localDBDir)
+def sourceDir = Paths.get("/home/CARD/localDB")
+// Copy files
+
+Files.list(sourceDir).each { sourceFile ->
+    if (Files.isRegularFile(sourceFile)) {
+        def targetFile = localDBDir.resolve(sourceFile.fileName.toString())
+        Files.copy(sourceFile, targetFile, StandardCopyOption.REPLACE_EXISTING)
+        println "Copied ${sourceFile.fileName} to ${localDBDir.toAbsolutePath()}"
+    }
+}
+
+Path cardOutputPath = tmpPath.resolve( "${cardOutput}.json" )
+
+ProcessBuilder pb = new ProcessBuilder(
+    '/bin/bash',
+    '-c', """
+    . /opt/conda/etc/profile.d/conda.sh && \
+    conda activate ${condaEnvName} && \
+    rgi main --input_type contig --num_threads 1 \
+    --input_sequence ${localGenomeSequencePath} \
+    --output_file ${cardOutput} --exclude_nudge --local
+    """
+    )
+    .redirectErrorStream( true )
+    .redirectOutput( ProcessBuilder.Redirect.INHERIT )
+    .directory( tmpPath.toFile() )
+
+log.info( "exec: ${pb.command()}" )
+log.info( '----------------------------------------------------------------------------------------------' )
+if( pb.start().waitFor() != 0 ) terminate( 'could not exec CARD rgi!', abrPath, genomeName )
+log.info( '----------------------------------------------------------------------------------------------' )
+
+
+// parse and aggregate CARD output
+def p = ~/([a-zA-Z]+) antibiotic/
+def abrs = [:]
+
+(new JsonSlurper()).parseText( cardOutputPath.text ).each( { cardHit ->
+    if( !cardHit.value[ 'data_type' ] ) {
+        cardHit.value.values().findAll( { !it?.nudged } ) // skip "nudged" CARD entries
+        .each( { hsp ->
+            def tmp = hsp.orf_from.split('_')
+            tmp = tmp[0..-2]
+            String contig = tmp.join('_')
+            def abr = [
+                model: [
+                    aroId: hsp.ARO_accession,
+                    name: hsp.model_name,
+                    desc: aroTerms.find( { it.accession.split(':')[1] == hsp.ARO_accession } ).description ?: '-',
+                    type: hsp.model_type,
+                    bitScore: hsp.pass_bitscore
+                ],
+                eValue: hsp.evalue,
+                bitScore: hsp[ 'bit_score' ],
+                percentSeqIdentity: hsp.perc_identity / 100,
+                alignment: hsp.match,
+                orf: [
+                    start: hsp.orf_start,
+                    end: hsp.orf_end,
+                    length: Math.abs( hsp.orf_start - hsp.orf_end ) + 1,
+                    strand: hsp.orf_strand
+                ],
+                antibiotics: [],
+                drugClasses: []
+            ]
+            if( hsp.snp ) {
+                abr.snp = hsp.snp
+            }
+            hsp.ARO_category.each( {
+                def aroCat = it.value
+                if( aroCat.category_aro_class_name == 'Drug Class' ) {
+                    abr.drugClasses << aroCat.category_aro_name - ' antibiotic'
+                } else if( aroCat.category_aro_class_name == 'Antibiotic' ) {
+                    abr.antibiotics << aroCat.category_aro_name
+                }
+
+            } )
+            abr.antibiotics = abr.antibiotics.toUnique().sort()
+            abr.drugClasses = abr.drugClasses.toUnique().sort()
+            if( abr.percentSeqIdentity > PERC_SEQ_IDENT ) {
+                if( abrs.containsKey( abr.orf.start ) ) {
+                    abrs[ (abr.orf.start) ] << abr
+                } else {
+                    abrs[ (abr.orf.start) ] = [ abr ]
+                }
+            }
+        } )
+    }
+} )
+
+// extract perfect hits
+def perfectHits = []
+def additionalHits = []
+abrs.each( { k, v ->
+    def perfectHit = v.find( { it.percentSeqIdentity == 1d } )
+    if( perfectHit ) {
+        perfectHits << perfectHit
+    } else {
+        v.each( { additionalHits << it } )
+    }
+} )
+
+
+// sort hits lists
+info.abr.perfect = perfectHits.sort( { it.orf.start } )
+info.abr.additional = additionalHits.sort( { a, b -> a.orf.start <=> b.orf.start ?: b.percentSeqIdentity <=> a.percentSeqIdentity ?: a.eValue <=> b.eValue } )
+
+
+// store info.json
+info.time.end = OffsetDateTime.now().toString()
+File infoJson = abrPath.resolve( "${genomeName}.json" ).toFile()
+infoJson << JsonOutput.prettyPrint( JsonOutput.toJson( info ) )
+
+
+// set state-file to finished
+Files.move( abrPath.resolve( "${genomeName}.running" ), abrPath.resolve( "${genomeName}.finished" ) )
+
+
+
+
+/**********************
+ *** Script Methods ***
+**********************/
+
+
+private void terminate( String msg, Path abrPath, String genomeName ) {
+    terminate( msg, null, abrPath, genomeName )
+}
+
+private void terminate( String msg, Throwable t, Path abrPath, String genomeName ) {
+
+    if( t ) log.error( msg, t )
+    else    log.error( msg )
+    Files.move( abrPath.resolve( "${genomeName}.running" ), abrPath.resolve( "${genomeName}.failed" ) ) // set state-file to failed
+    System.exit( 1 )
+
+}
---
+++ asap-singularity.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+
+######################################################
+# ASA³P bash wrapper script for Docker
+#
+# Use this bash wrapper script in order to forestall common
+# issues with paths and Docker related options.
+# Also make sure to leave this script within the ASA³P directory, as by this,
+# the exact path to the ASA³P directory is auto-detected and forwarded, correctly.
+######################################################
+
+set -e
+ASAP=""
+DATA=""
+SCRATCH=""
+SKIP_CHAR=""
+SKIP_COMP=""
+DEBUG=""
+
+usage() {
+  echo "Usage: $0 -p <PROJECT_DIR> [-a <ASAP_DIR>] [-s <SCRATCH_DIR>] [-z] [-c] [-d] [-h]" 1>&2
+}
+
+
+exit_abnormal() {
+  echo -e "\nFor further information, please have a look at: https://github.com/oschwengers/asap"
+  exit 1
+}
+
+
+while getopts ":p:a:s:zcdh" options; do
+    case "${options}" in
+        p)
+            if [[ "$OPTARG" == -* ]]; then
+                usage
+                echo "Error: -p requires an argument!"
+                exit_abnormal
+            else
+                DATA=${OPTARG}
+            fi
+            ;;
+        a)
+            if [[ "$OPTARG" == -* ]]; then
+                usage
+                echo "Error: -a requires an argument!"
+                exit_abnormal
+            else
+                ASAP=${OPTARG}
+            fi
+            ;;
+        s)
+            if [[ "$OPTARG" == -* ]]; then
+                usage
+                echo "Error: -s requires an argument!"
+                exit_abnormal
+            else
+                SCRATCH=${OPTARG}
+            fi
+            ;;
+        z)
+            SKIP_CHAR="--skip-char"
+            echo "Skip characterization steps"
+            ;;
+        c)
+            SKIP_COMP="--skip-comp"
+            echo "Skip comparative steps"
+            ;;
+        d)
+            DEBUG="--debug"
+            echo "Enable debugging output: $DEBUG"
+            ;;
+        h)
+            usage && exit 0;;
+        :) # expected argument omitted:
+            usage
+            echo "Error: -${OPTARG} requires an argument!"
+            exit_abnormal
+            ;;
+        ?) # unknown option:
+            usage
+            echo "Error: unknown argument (${OPTARG}) detected!"
+            exit_abnormal
+            ;;
+    esac
+done
+
+
+# set ASAP_DIR to default path and/or normalize path
+if [ "$ASAP" = "" ]; then # if $ASAP was _not_ set by the user
+    SCRIPT_PATH=$(realpath $0)
+    ASAP=$(dirname $SCRIPT_PATH)
+else
+    ASAP=$(realpath $ASAP)
+fi
+
+
+# test existence of the ASAP directory
+if [ ! -f $ASAP/asap.jar ]; then
+    echo "ASA³P directory (${ASAP}) does not exist or seems to be corrupt!"
+    echo "Is this script stored withtin the ASA³P directory? If not, please specify the absolute path to it via '-a <ASAP_DIR>' or copy it into the ASA³P directory."
+    exit_abnormal
+else
+    echo "ASA³P:   ${ASAP}"
+fi
+
+
+if [ "$DATA" = "" ]; then # if $DATA was _not_ set by the user
+    echo "Project directory must be specified!"
+    usage
+    exit_abnormal
+fi
+
+
+# test existence of the project directory
+if [ ! -d $DATA ]; then
+    echo "Project directory (${DATA}) does not exist!"
+    exit_abnormal
+else
+    DATA=$(realpath $DATA)
+    echo "DATA:    ${DATA}"
+fi
+
+
+# test existance of the config file
+if [ ! -f $DATA/config.xls ]; then
+    echo "No config file in project directory detected!"
+    echo -e "\nPlease, provide a proper 'config.xls' file within the project directory."
+    exit_abnormal
+fi
+
+
+# test existance of the data subdirectory
+if [ ! -d $DATA/data ]; then
+    echo "No data subdirectory in project directory detected!"
+    echo -e "\nPlease, provide a proper 'data' subdirectory within the project directory, containing all reference and isolate related files."
+    exit_abnormal
+fi
+
+
+# normalize SCRATCH if set by the user
+if [ "$SCRATCH" != "" ]; then # if $SCRATCH _was_ set by the user
+    if [ ! -d $SCRATCH ]; then
+        echo "Scratch directory (${SCRATCH}) does not exist!"
+        exit_abnormal
+    else
+        SCRATCH=$(realpath $SCRATCH)
+        echo "Scratch: $SCRATCH"
+    fi
+fi
+
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
+
+if [ "$SCRATCH" != "" ]; then
+    singularity run -B \
+    $ASAP:/asap:ro,$DATA:/data,$SCRATCH:/var/scratch,/etc/group:/etc/group:ro,/etc/passwd:/etc/passwd:ro \
+    asap_latest.sif \
+    $SKIP_CHAR $SKIP_COMP $DEBUG
+else
+    singularity run -B \
+    $ASAP:/asap:ro,$DATA:/data,/etc/group:/etc/group:ro,/etc/passwd:/etc/passwd:ro \
+    asap_latest.sif \
+    $SKIP_CHAR $SKIP_COMP $DEBUG
+fi
+
+
+# exit with 'docker run' exit code
+exit $?
